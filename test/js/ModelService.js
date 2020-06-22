1be2d5a83761faf9afea0088c0561d840c290ca8
jzt123 <464615984@qq.com>
2020-06-22 11:32:49 +0800

    14jiangzhentao

---
+++ test/js/ModelService.js
@@ -0,0 +1,64 @@
+
+  var model = angular.module('nameList.model', []);
+
+    function Guest(name, phone) {
+        this.name = name;
+        this.phone = phone;
+        this.state = Guest.INVITE;
+    }
+   
+    Guest.INVITE = '邀请中';
+    Guest.REFUSE = '已拒绝';
+    Guest.ALL = '全部';
+
+
+    Guest.prototype.accept = function () {
+        this.state = Guest.ACCEPT;
+    }
+
+    Guest.prototype.refuse = function () {
+        this.state = Guest.REFUSE;
+    }
+
+    
+    model.factory('modelService', function () {
+        var guestList = {
+            list: [],
+            add: function (name, phone) {
+                var isok = true;
+                isok = !!(isok && name && phone);
+                if (!isok) {
+                    return {
+                        code: 1,
+                        guest: null
+                    }
+                }
+
+            //添加
+                var guest = new Guest(name, phone);
+                this.list.push(guest);
+                return {
+                    code: 0,
+                    guest: guest
+                }
+            },
+            remove: function (guest) {
+                this.list = this.list.filter(function (item) {
+                    return guest.phone != item.phone;
+                })
+            },
+
+
+            getList:function(state){
+                if (state==Guest.ALL){
+                    return this.list.filter(function(){
+                        return true;
+                    })
+                }
+                return this.list.filter(function (guest){
+                    return guest.state==state;
+                })
+            }
+        }
+        return guestList;
+    })
